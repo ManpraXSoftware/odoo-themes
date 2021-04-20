cf79f4a81419444a33b2956c50f879862b5af090
Jitendra Soni <jitendra.soni@manprax.com>
2021-04-20 14:03:38 +0530

    created new modules and changed ui colors

---
+++ odoo_theme/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Odoo Theme',
+    'description': 'Odoo Theme',
+    'category': 'Theme/Services',
+    'summary': 'Corporate, Business, Tech, Services',
+    'sequence': 120,
+    'version': '2.0',
+    'author': 'Manprax Software LLP',
+    'depends': ['base'],
+    'data': [
+        'views/templates.xml'
+    ],
+    'images': [],
+    'license': 'LGPL-3',
+}
